e92dcf1a4e27bc10f6d081960fdfa8e7f812e471
MorningPants <77476821+MorningPants@users.noreply.github.com>
2022-06-09 13:34:42 -0700

    working reviews API

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const PORT = 8000;
+
+const reviews = {
+  andy: {
+    stars: 5,
+    text: "Andy is great!",
+  },
+  callum: {
+    stars: 5,
+    text: "Callum is great!",
+  },
+};
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/api/:name", (req, res) => {
+  const name = req.params.name;
+  if (reviews[name]){
+
+      res.json(reviews[name]);
+    }
+    else{
+        res.sendStatus('404')
+    }
+});
+
+app.listen(PORT, () => {
+  console.log(`The server is running on port ${PORT}.`);
+});
